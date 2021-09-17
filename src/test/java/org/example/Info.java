950327145143bc33e3ffe84be978e2a61a5ddf5c
Rodion <rodionchik17@gmail.com>
2021-09-17 22:15:56 +0300

    Refactoring : info currency

--- src/test/java/org/example/Info.java
+++ src/test/java/org/example/Info.java
@@ -12,7 +12,7 @@ public class Info {
     public static final String EUR = "EUR";
 
     public static final String ₴ = "₴";
-    public static final String $ = "\\$";
+    public static final String $ = "$";
     public static final String € = "€";
 
     public static final String REGEXCURRENCY = "[" + $ + ₴ + € + "]";
